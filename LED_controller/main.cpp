0029d44ff4195e606b295c2f76a15e3ee90d4fb5
Nikolay <kurkin.nik@yandex.ru>
2024-05-25 17:52:09 +0300

    Рабочая версия с перерываниями
    Рабочая версия с перерываниями

--- LED_controller/main.cpp
+++ LED_controller/main.cpp
@@ -7,8 +7,10 @@
 #include "DS1307.h"
 #include "ST7789.h"
 
+
 // Флаги
-char syncflag = 1;
+unsigned char flagsync = 1;
+unsigned char flagsetting = 3;
 
 
 // Переменные экрана
@@ -19,10 +21,13 @@ char red = 60;
 char green = 55;
 char blue = 45;
 
+
 // Переменные времени
-char time[7] = {0, 8, 15, 4, 23, 5, 24};											// sec, min, hour, day, date, month, year
+char time[7] = {0, 0, 8, 4, 23, 5, 24};											// sec, min, hour, day, date, month, year
+char timesetting[7];
 float timeDec = (float)time[0] + (float)time[1] / 60.0 + (float)time[2] / 3600.0;	// Реальное время в десятичном виде
 char timecomp = time[0];															// Секунда сравнения
+
 
 char timeschedule[4][2] =						// Расписание
 {
@@ -41,14 +46,20 @@ float timescheduleDec[4]=
 };
 
 char wordschedule[4][8] = {"Рассвет", "День   ", "Закат  ", "Ночь   "};
+
+	
 	
 // Функции
 void setup(void);
-void printClock(void);
+void printClock(char time[]);
 void printSchedule(void);
 inline void reverse(char* str, int len);
 inline int intToStr(int x, char str[], int d);
 char* ftoa(float n, char* res, int afterpoint);
+
+
+
+
 
 int main(void)
 {
@@ -56,84 +67,121 @@ int main(void)
 	setup();
 	ST7789::fillScreen(0, 0, 0);
 	
-	//DS1307::writeTime(time);
+	
+	/*
+	DS1307::writeTime(time);
 	_delay_ms(50);
+	*/
+	
 	
 	DS1307::readTime(time);
 		
+		
+		
 	while (1) 
     {
-		while (time[0] == timecomp){}
+		while (time[0] == timecomp && flagsetting == 3){}					// Установка обьновления экрана - без задержки или каждую секунду
 		timecomp = time[0];	
 		
+		
+		//==============================================================================
+		//									Блок времени
+		//==============================================================================		
 		xpos = 0;
 		ypos = 0;
-		printClock();
+		
+		if (flagsetting == 3)
+		{
+			printClock(time);
+		}
+		else
+		{
+			printClock(timesetting);
+		}
 		
 		
 		
+		
+		//==============================================================================
+		//								Блок времени суток
+		//==============================================================================
 		xpos = 0;
 		ypos = 40;
 		
-		// Расчеты времён		
-		timeDec = (float)time[2] + (float)time[1] / 60.0 + (float)time[0] / 3600.0;
-		
-		for(char i = 0; i < 4; i++)
+		if (flagsetting == 3)
 		{
-			timescheduleDec[i] = (float)timeschedule[i][0] + (float)timeschedule[i][1] / 60.0;
+			// Расчеты времён
+			timeDec = (float)time[2] + (float)time[1] / 60.0 + (float)time[0] / 3600.0;
+					
+			for(char i = 0; i < 4; i++)
+			{
+				timescheduleDec[i] = (float)timeschedule[i][0] + (float)timeschedule[i][1] / 60.0;
+			}
+					
+			// Определение слова и скважности
+			if (timeDec < timescheduleDec[0])													// Время меньше чем время рассвета = ночь - 3
+			{
+				ST7789::printString(wordschedule[3], &xpos, &ypos, red, green, blue, 5);
+				OCR0 = 0;
+			}
+			else if (timeDec < timescheduleDec[1])												// Время меньше чем время начала дня = рассвет - 0
+			{
+				ST7789::printString(wordschedule[0], &xpos, &ypos, red, green, blue, 5);
+				OCR0 = (char)(((timeDec - timescheduleDec[0])/(timescheduleDec[1] - timescheduleDec[0])) * 255.0);
+			}
+			else if (timeDec < timescheduleDec[2])												// Время меньше чем время конца дня = день - 1
+			{
+				ST7789::printString(wordschedule[1], &xpos, &ypos, red, green, blue, 5);
+				OCR0 = 255;
+			}
+			else if (timeDec < timescheduleDec[3])												// Время меньше чем время конца заката = закат - 2
+			{
+				ST7789::printString(wordschedule[2], &xpos, &ypos, red, green, blue, 5);
+				OCR0 = (char)((1.0 - ((timeDec - timescheduleDec[2])/(timescheduleDec[3] - timescheduleDec[2]))) * 255.0);
+			}
+			else																				// В остальных случаях ночь - 3
+			{
+				ST7789::printString(wordschedule[3], &xpos, &ypos, red, green, blue, 5);
+				OCR0 = 0;
+			}
 		}
 		
-
-
-		// Определение слова и скважности
-		if (timeDec < timescheduleDec[0])													// Время меньше чем время рассвета = ночь - 3
-		{
-			ST7789::printString(wordschedule[3], &xpos, &ypos, red, green, blue, 5);
-			OCR0 = 0;
-		}
-		else if (timeDec < timescheduleDec[1])												// Время меньше чем время начала дня = рассвет - 0
-		{
-			ST7789::printString(wordschedule[0], &xpos, &ypos, red, green, blue, 5);
-			OCR0 = (char)(((timeDec - timescheduleDec[0])/(timescheduleDec[1] - timescheduleDec[0])) * 255.0);
-		}
-		else if (timeDec < timescheduleDec[2])												// Время меньше чем время конца дня = день - 1
-		{
-			ST7789::printString(wordschedule[1], &xpos, &ypos, red, green, blue, 5);
-			OCR0 = 255;
-		}
-		else if (timeDec < timescheduleDec[3])												// Время меньше чем время конца заката = закат - 2
-		{
-			ST7789::printString(wordschedule[2], &xpos, &ypos, red, green, blue, 5);
-			OCR0 = (char)((1.0 - ((timeDec - timescheduleDec[2])/(timescheduleDec[3] - timescheduleDec[2]))) * 255.0);
-		}
-		else																				// В остальных случаях ночь - 3
-		{
-			ST7789::printString(wordschedule[3], &xpos, &ypos, red, green, blue, 5);
-			OCR0 = 0;
-		}
 		
 		
 		
-		
-		
+		//==============================================================================
+		//								Блок скважности
+		//==============================================================================
 		xpos = 0;
 		ypos = 85;
 		
-		ST7789::printString(itoa(OCR0, buffer, 10), &xpos, &ypos, red, green, blue, 3);
-		ST7789::printString(" ", &xpos, &ypos, red, green, blue, 3);
-		
-		if ((float)OCR0/2.55 < 10)
+		if (flagsetting == 3)
 		{
-			ST7789::printString("0", &xpos, &ypos, red, green, blue, 3);
+			ST7789::printString(itoa(OCR0, buffer, 10), &xpos, &ypos, red, green, blue, 3);		// Печать OCR0
+					
+			ST7789::printString("осв.", &xpos, &ypos, red, green, blue, 3);						// Печать осв.	
+					
+			if ((float)OCR0/2.55 < 10)															// Печать процента
+			{
+				ST7789::printString("0", &xpos, &ypos, red, green, blue, 3);
+			}
+					
+			ST7789::printString(ftoa(((float)OCR0/2.55), buffer, 2), &xpos, &ypos, red, green, blue, 3);
+			ST7789::printString("%  ", &xpos, &ypos, red, green, blue, 3);
 		}
 		
-		ST7789::printString(ftoa(((float)OCR0/2.55), buffer, 2), &xpos, &ypos, red, green, blue, 3);
-		ST7789::printString("%  ", &xpos, &ypos, red, green, blue, 3);
 		
+		
+		//==============================================================================
+		//								Блок расписания
+		//==============================================================================
 		xpos = 0;
 		ypos = 130;
-		printSchedule();
-    }
+		if (flagsetting == 3)
+		{
+			printSchedule();
+		}
+	}
 }
 
 
@@ -166,15 +214,15 @@ ISR(TIMER1_COMPA_vect)
 	
 	
 	// Синхронизация с DS1307 в полночь
-	if (time[0] == 0 && time[1] == 0 && time[2] == 0 && syncflag == 1)
+	if (time[0] == 0 && time[1] == 0 && time[2] == 0 && flagsync == 1)
 	{
 		DS1307::readTime(time);
-		syncflag = 0;
+		flagsync = 0;
 	}
 	
 	if (time[0] == 0 && time[1] == 1 && time[2] == 0)
 	{
-		syncflag = 1;
+		flagsync = 1;
 	}
 	
 	
@@ -183,24 +231,155 @@ ISR(TIMER1_COMPA_vect)
 
 ISR(INT0_vect)
 {
-	time[0]++;
+	cli();
+	
+	_delay_ms(20);
+	if (PIND & (1 << PIND2))		// Если высокий уровень
+	{
+		return;
+	}
+		
+	if (flagsetting == 0)
+	{
+		timesetting[0]++;
+		if (timesetting[0] >= 60)
+		{
+			timesetting[0] = 0;
+		}
+	}
+	
+	if (flagsetting == 1)
+	{
+		timesetting[1]++;
+		if (timesetting[1] >= 60)
+		{
+			timesetting[1] = 0;
+		}
+	}
+	
+	if (flagsetting == 2)
+	{
+		timesetting[2]++;
+		if (timesetting[2] >= 24)
+		{
+			timesetting[2] = 0;
+		}
+	}
+	
+	if (flagsetting == 3)
+	{
+		return;
+	}
+	
+	PORTA = 1;
+	_delay_ms(20);
+	PORTA = 0;
+	
+	sei();
 }
 
 ISR(INT1_vect)
 {
-	time[0]--;
+	cli();
+	
+	_delay_ms(20);
+	if (PIND & (1 << PIND3))		// Если высокий уровень
+	{
+		return;
+	}
+	
+	
+	if (flagsetting == 0)
+	{
+		timesetting[0]--;
+		
+		if (timesetting[0] >= 60)			// потому что 255
+		{
+			timesetting[0] = 59;
+		}
+	}
+	
+	if (flagsetting == 1)
+	{
+		timesetting[1]--;
+		
+		if (timesetting[1] >= 60)
+		{
+			timesetting[1] = 59;
+		}
+	}
+	
+	if (flagsetting == 2)
+	{
+		timesetting[2]--;
+		
+		if (timesetting[2] >= 24)
+		{
+			timesetting[2] = 23;
+		}
+	}
+	
+	if (flagsetting == 3)
+	{
+		return;
+	}
+	
+	
+	
+	PORTA = 1;
+	_delay_ms(20);
+	PORTA = 0;
+	
+	sei();
 }
 
 ISR(INT2_vect)
-{
-	DS1307::writeTime(time);
+{	
+	cli();
+	
+	_delay_ms(50);
+	if (PINB & (1 << PINB2))		// Если высокий уровень
+	{
+		return;
+	}
+		
+	flagsetting++;
+	if (flagsetting >= 4)
+	{
+		flagsetting = 0;
+	}
+	
+	
+	if (flagsetting == 0)					// При начале настройки загрузить текущее время в буффер
+	{
+		for(int i = 0; i < 7; i++)
+		{
+			timesetting[i] = time[i];
+		}
+	}
+	
+	
+	if (flagsetting == 3)					// При окончании настройки выгрузить время из буффера
+	{
+		for(int i = 0; i < 7; i++)
+		{
+			time[i] = timesetting[i];
+		}
+		DS1307::writeTime(time);
+	}
+
+	
+	PORTA = 1;
+	_delay_ms(20);
+	PORTA = 0;
+	
+	sei();
 }
 
 
 void setup(void)
 {
 	sei();
-	
 	
 	////////////// Внешние прерывания //////////////
 	GICR |= (1 << INT1)|(1 << INT0)|(1 << INT2);						// General Interrupt Control Register - Установка битов INT1, INT0 или INT2 разрешает прерывания при возникновении события на соответствующем выводе микроконтроллера AVR, а сброс — запрещает.
@@ -227,29 +406,71 @@ void setup(void)
 	ST7789::init();
 }
 
-void printClock(void)
+void printClock(char time[])
 {
-	if (time[2] < 10)
+	
+	
+	if (flagsetting == 2)
 	{
-		ST7789::printString("0", &xpos, &ypos, red, green, blue, 5);
+		if (time[2] < 10)
+		{
+			ST7789::printString("0", &xpos, &ypos, red, 0, 0, 5);
+		}
+		ST7789::printString(itoa(time[2], buffer, 10), &xpos, &ypos, red, 0, 0, 5);
 	}
-	ST7789::printString(itoa(time[2], buffer, 10), &xpos, &ypos, red, green, blue, 5);
+	else
+	{
+		if (time[2] < 10)
+		{
+			ST7789::printString("0", &xpos, &ypos, red, green, blue, 5);
+		}
+		ST7789::printString(itoa(time[2], buffer, 10), &xpos, &ypos, red, green, blue, 5);
+	}
+	
+	
 	ST7789::printString(":", &xpos, &ypos, red, green, blue, 5);
 	
-	if (time[1] < 10)
+	
+	if (flagsetting == 1)
 	{
-		ST7789::printString("0", &xpos, &ypos, red, green, blue, 5);
+		if (time[1] < 10)
+		{
+			ST7789::printString("0", &xpos, &ypos, red, 0, 0, 5);
+		}
+		ST7789::printString(itoa(time[1], buffer, 10), &xpos, &ypos, red, 0, 0, 5);
 	}
-	ST7789::printString(itoa(time[1], buffer, 10), &xpos, &ypos, red, green, blue, 5);
+	else
+	{
+		if (time[1] < 10)
+		{
+			ST7789::printString("0", &xpos, &ypos, red, green, blue, 5);
+		}
+		ST7789::printString(itoa(time[1], buffer, 10), &xpos, &ypos, red, green, blue, 5);
+		
+	}
+	
 	ST7789::printString(":", &xpos, &ypos, red, green, blue, 5);
 	
-	if (time[0] < 10)
+	if (flagsetting == 0)
 	{
-		ST7789::printString("0", &xpos, &ypos, red, green, blue, 5);
+		if (time[0] < 10)
+		{
+			ST7789::printString("0", &xpos, &ypos, red, 0, 0, 5);
+		}
+		ST7789::printString(itoa(time[0], buffer, 10), &xpos, &ypos, red, 0, 0, 5);
 	}
-	ST7789::printString(itoa(time[0], buffer, 10), &xpos, &ypos, red, green, blue, 5);
+	else
+	{
+		if (time[0] < 10)
+		{
+			ST7789::printString("0", &xpos, &ypos, red, green, blue, 5);
+		}
+		ST7789::printString(itoa(time[0], buffer, 10), &xpos, &ypos, red, green, blue, 5);
+	}
 	
 }
+
+
 
 void printSchedule(void)
 {
